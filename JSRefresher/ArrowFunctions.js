eda01cb56c3fd621c349d3dded7e557d56608523
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-15 16:05:38 +0100

    Learning about arrow functions

---
+++ JSRefresher/ArrowFunctions.js
@@ -0,0 +1,14 @@
+
+//Arrow function and and normal function do same, arrow function is just another way
+// to write function
+
+function printName(name) {
+    console.log(name)
+}
+
+const printLastName = (lastName) =>{
+    console.log(lastName)
+}
+
+printName("Krsto")
+printLastName("Kostic")
