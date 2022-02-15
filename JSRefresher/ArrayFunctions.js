92789a2da07257837eed6cf36778a62687a0b69e
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-15 21:05:11 +0100

    Function to manipulate with each element in array

---
+++ JSRefresher/ArrayFunctions.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3];
+
+// This map will go through every element and we can manipulate with each element in array
+const doubleNumArray = numbers.map((num) =>{
+    return num * 2;
+})
+
+console.log(doubleNumArray)
