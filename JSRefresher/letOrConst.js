750c5af6648bc0ca5e003c93134a51ac11163953
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-15 15:53:51 +0100

    Learning about difference between let and const

---
+++ JSRefresher/letOrConst.js
@@ -0,0 +1,11 @@
+// With let or var we can reassign our variable
+let myName = "Krsto"
+myName = "Krsto2";
+console.log(myName);
+
+//With const we will get error because we tryed to reassing constant variable
+
+const myLastName = "Kostic";
+
+myLastName = "Kostic2"
+console.log(myLastName)
