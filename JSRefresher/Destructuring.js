2e420fceedf4f5ae9ad6225f12a3269e3b7c04fb
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-15 20:58:04 +0100

    Practise destructoring in array

---
+++ JSRefresher/Destructuring.js
@@ -0,0 +1,8 @@
+// We use destructoring to extract array element or object property and store them in variable
+
+const numbers = [1,2,3];
+
+// We extract first and sacond element from array
+[n1,n2] = numbers;
+
+console.log(n1,n2)
