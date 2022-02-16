a52ecb94076169dd932abeb005d88b6361d5ea1c
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-16 09:54:10 +0100

    Making shell component and learning what is children props, and how not to duplicate code

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 function App() {
-  const expenses = [
-    {title: "Car Insurance", amount: 294.67, date: new Date(2021,2,8)},
-    {title: "Car Insurance", amount: 294.67, date: new Date(2021,2,8)},
-    {title: "Car Insurance", amount: 294.67, date: new Date(2021,2,8)},
-    {title: "Car Insurance", amount: 294.67, date: new Date(2021,2,8)}
-  ]
+  
   return (
     <div className="App">
-      {expenses.map((data,id) =>{
-        return <ExpenseItem info={data} key={id}/>
-      })}
+     <Expenses />
     </div>
   );
 }
--- reactapp/src/components/ExpenseItem.js
+++ reactapp/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props){
-
-    return(
-     <div className="expense-item">
-         <ExpenseDate date={props.info.date}/>
-         <div className="expense-item__description">
-             <h2>{props.info.title}</h2>
-             <div className="expense-item__price">${props.info.amount}</div>
-         </div>
-     </div>   
-    )
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.info.date} />
+      <div className="expense-item__description">
+        <h2>{props.info.title}</h2>
+        <div className="expense-item__price">${props.info.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
