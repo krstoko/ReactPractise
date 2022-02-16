9bd8a33c704b78afc1eea11345e25c1f32ba46d3
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-16 20:22:42 +0100

    Installing and learning styled components. Refactoring my code without css and change it into styled components.

--- reactapp/src/components/Chart/Chart.js
+++ reactapp/src/components/Chart/Chart.js
@@ -1,14 +1,32 @@
 import ChartBar from "./ChartBar";
+import styled from "styled-components";
+const ChartStyle = styled.div`
+  padding: 1rem;
+  border-radius: 12px;
+  background-color: #f8dfff;
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  height: 10rem;
+`;
+
 
 const Chart = (props) => {
-    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
-    const totalMaximum = Math.max(...dataPointValues)
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
   return (
-    <div className="chart">
+    <ChartStyle>
       {props.dataPoints.map((dataPoint) => {
-        return <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label}/>;
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={totalMaximum}
+            label={dataPoint.label}
+          />
+        );
       })}
-    </div>
+    </ChartStyle>
   );
 };
 
--- reactapp/src/components/Chart/ChartBar.js
+++ reactapp/src/components/Chart/ChartBar.js
@@ -1,22 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 
+const CharBarStyle = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const FillChartBar = styled.div`
+  background-color: #4826b9;
+  width: 100%;
+  transition: all 0.3s ease-out;
+  height: ${(props) => props.height};
+`;
+
+const ChartInner = styled.div`
+  height: 100%;
+  width: 100%;
+  border: 1px solid #313131;
+  border-radius: 12px;
+  background-color: #c3b4f3;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+const ChartLabel = styled.div`
+  font-weight: bold;
+  font-size: 0.5rem;
+  text-align: center;
+`;
 const ChartBar = (props) => {
   let barFillHeight = "0%";
 
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
-  
+
   return (
-    <div className="chart-bar">
-      <div className="chart-bar__inner">
-        <div
-          className="chart-bar__fill"
-          style={{ height: barFillHeight }}
-        ></div>
-      </div>
-      <div className="chart-bar__label">{props.label}</div>
-    </div>
+    <CharBarStyle>
+      <ChartInner>
+        <FillChartBar height={barFillHeight}></FillChartBar>
+      </ChartInner>
+      <ChartLabel>{props.label}</ChartLabel>
+    </CharBarStyle>
   );
 };
 
