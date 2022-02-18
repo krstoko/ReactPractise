9006be94105f893f288e9c7f1c750091c010725f
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-18 16:37:09 +0100

    Styling and adding bump animation on add or remove item from chart. With this we finished projecet

--- food-app/src/components/Layout/HeaderCartButton.js
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    let timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
   return (
-    <button className={classes.button} onClick={props.showCartHandler}>
+    <button className={btnClasses} onClick={props.showCartHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
