ba8e78b58c2e59c34fa898029a24722498901bc2
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-18 16:08:21 +0100

    Adding complex reducer for adding more food into cart and removing food from cart

--- food-app/src/components/Cart/Cart.js
+++ food-app/src/components/Cart/Cart.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 const Cart = (props) => {
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+  const cartCtx = useContext(CartContext);
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
-      ))}
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        );
+      })}
     </ul>
   );
+
+  const totalAmoint = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
   return (
     <Modal hideCartHandler={props.hideCartHandler}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amunt</span>
-        <span>35.62</span>
+        <span>{totalAmoint}</span>
       </div>
       <div className={classes.actions}>
         <button
@@ -23,7 +44,7 @@ const Cart = (props) => {
         >
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- food-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmiuntNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === "" ||
+      enteredAmiuntNumber < 1 ||
+      enteredAmiuntNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.addToCartHandler(enteredAmiuntNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        label={`Amount ${props.id}`}
+        label="Amount"
+        ref={amountInputRef}
         input={{
-          id: "",
+          id: `id ${props.id}`,
           type: "number",
           min: "1",
           max: "5",
@@ -16,6 +35,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please add valid amount</p>}
     </form>
   );
 };
