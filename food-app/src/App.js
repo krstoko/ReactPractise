11b73e094082ecdd1cf305738a7dc07e6ba5c855
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-18 14:45:28 +0100

    Adding context provider and setting context

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShowm, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShowm && <Cart hideCartHandler={hideCartHandler} />}
       <Header showCartHandler={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- food-app/src/components/Layout/HeaderCartButton.js
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.showCartHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
