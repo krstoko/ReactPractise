7b69a2566f088e73fb50f044001c3683d4909652
Krsto Kostic <krstoko@maestralsolutions.com>
2022-02-20 18:51:17 +0100

    Learning what is http request and how to fetch data from backend using fetch

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
 
+  const fetchMovieHandler = () => {
+    fetch("https://swapi.py4e.com/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const tranformedMovies = data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl,
+            releaseDate: movie.release_data,
+          };
+        });
+        setMovies(tranformedMovies);
+      });
+  };
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
